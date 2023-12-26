ef7d7e101cdf02d05419aa5b28cff1a495bef372
ahmeddadel7 <147009800+vodz1@users.noreply.github.com>
2023-12-26 15:30:12 +0200

    Update lab1.js

--- lab1.js
+++ lab1.js
@@ -1,9 +1,9 @@
-/*let firstName = window.prompt("Enter your first name");
+let firstName = window.prompt("Enter your first name");
 let lastName = window.prompt("Enter your last name");
 
 let birthyear = window.prompt("Enter your birthday year");
 let years = 2023 - Number(birthyear);
-alert(`welcome to this page ${firstName} ${lastName} , you are ${years} years old`);*/
+alert(`welcome to this page ${firstName} ${lastName} , you are ${years} years old`);
 
 alert(
   "its the first release of a calculator that only has a summation feature."
