d3b53ebe141e81a0e5c979c684baba9bcf643465
ahmeddadel7 <147009800+vodz1@users.noreply.github.com>
2024-01-04 20:51:22 +0200

    Update lab3.js

--- lab 3 js/lab3.js
+++ lab 3 js/lab3.js
@@ -61,3 +61,75 @@ switch (choice) {
     break;
 }
 */
+
+
+
+// PART 2
+/*
+let input = Number(prompt("How many numbers do you want to enter"));
+let arr = [];
+var sum = 0;
+for (i = 0; i < input; i++) {
+  value = parseFloat(prompt(`Enter number ${i + 1}`));
+  arr[i] = value;
+  sum += arr[i];
+}
+let average = sum / input;
+document.write(`Sum = ${sum}`);
+document.write("<br>");
+document.write(`Average = ${average}`);
+*/
+
+// phone book
+// const contactArr = [];
+// function addContact() {
+//   const Contactname = prompt("Enter contact name");
+//   const Contactnumber = prompt("Enter contact number");
+
+//   const Contact = {
+//     name: Contactname,
+//     phoneNumber: Contactnumber,
+//   };
+//   contactArr.push(Contact);
+//   alert("Contact added successfully");
+// }
+
+// function searchContact() {
+//   const searchinput = prompt("Enter the conctact name or phone number");
+//   if (searchinput === null || searchinput === "") {
+//     alert("you haven't entered a name or a phone number");
+//   } else {
+//     for (const contact of contactArr) {
+//       if (
+//         contact.name.toLowerCase().includes(searchinput.toLowerCase()) ||
+//         contact.phoneNumber.includes(searchinput)
+//       ) {
+//         alert(
+//           `contact found !\n\n Name : ${contact.name}\n\n phone number : ${contact.phoneNumber}`
+//         );
+//         return;
+//       } else {
+//         alert("Contact not found !");
+//       }
+//     }
+//   }
+// }
+// while (true) {
+//   let input = prompt("Enter your option (add , search)");
+//   if (input === null) {
+//     alert("Goodbye!");
+//     break;
+//   } else {
+//     switch (input) {
+//       case "add":
+//         addContact();
+//         break;
+//       case "search":
+//         searchContact();
+//         break;
+//       default:
+//         alert("Choose an option");
+//     }
+//   }
+// }
+
